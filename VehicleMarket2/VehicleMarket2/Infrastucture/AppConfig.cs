0771b902b6543ba643ef323071d82a3b22c54fd2
toome <toome@LAPTOP-S683NICJ>
2019-05-03 22:09:05 +0200

    UrlHelper (facade) added. Starting to use it by dynamicly adding vehicles photos

---
+++ VehicleMarket2/VehicleMarket2/Infrastucture/AppConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace VehicleMarket2.Infrastucture
+{
+    public class AppConfig
+    {
+        private static string _categoryIconRelationFolder = ConfigurationManager.AppSettings["CategoryIconFolder"];
+
+        public static string CategoryIconRelationFolder
+        {
+            get { return _categoryIconRelationFolder; }
+        }
+
+        private static string _imagesIconRelationFolder = ConfigurationManager.AppSettings["ImagesIconFolder"];
+
+        public static string ImagesIconRelationFolder
+        {
+            get { return _imagesIconRelationFolder; }
+        }
+
+        private static string _vehiclesIconRelationFolder = ConfigurationManager.AppSettings["VehiclesIconFolder"];
+
+        public static string VehiclesIconRelationFolder
+        {
+            get { return _vehiclesIconRelationFolder; }
+        }
+    }
+}
---
+++ VehicleMarket2/VehicleMarket2/Infrastucture/UrlHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace VehicleMarket2.Infrastucture
+{
+    public static class UrlHelpers
+    {
+        public static string CategoriesIconsPath(this UrlHelper helper, string categoryIconName)
+        {
+            var CategoriesIconFolder = AppConfig.CategoryIconRelationFolder;
+            var tmpPath = Path.Combine(CategoriesIconFolder, categoryIconName);
+            var absPath = helper.Content(tmpPath);
+
+            return absPath;
+        }
+
+        public static string ImagesIconsPath(this UrlHelper helper, string imagesIconName)
+        {
+            var ImagesIconFolder = AppConfig.CategoryIconRelationFolder;
+            var tmpPath = Path.Combine(ImagesIconFolder, imagesIconName);
+            var absPath = helper.Content(tmpPath);
+
+            return absPath;
+        }
+
+        public static string VehiclesIconsPath(this UrlHelper helper, string vehicleIconName)
+        {
+            var VehiclesIconFolder = AppConfig.CategoryIconRelationFolder;
+            var tmpPath = Path.Combine(VehiclesIconFolder, vehicleIconName);
+            var absPath = helper.Content(tmpPath);
+
+            return absPath;
+        }
+    }
+}
